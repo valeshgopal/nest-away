f0c5d7df92e4f50c36e924638921e9a8199d50ff
Valesh Gopal <valesh.dev@gmail.com>
2025-01-29 19:58:48 +0530

    admin dashboard

--- utils/actions.ts
+++ utils/actions.ts
@@ -12,6 +12,7 @@ import {
 } from './schemas';
 import { uploadImage } from './supabase';
 import { calculateTotals } from './calculateTotals';
+import { formatDate } from './format';
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -620,4 +621,54 @@ export const fetchReservations = async () => {
     },
   });
   return reservations;
+};
+
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect('/');
+  return user;
+};
+
+export const fetchStats = async () => {
+  await getAdminUser();
+
+  const usersCount = await db.profile.count();
+  const propertiesCount = await db.property.count();
+  const bookingsCount = await db.booking.count();
+
+  return {
+    usersCount,
+    propertiesCount,
+    bookingsCount,
+  };
+};
+
+export const fetchChartsData = async () => {
+  await getAdminUser();
+  const date = new Date();
+  date.setMonth(date.getMonth() - 6);
+  const sixMonthsAgo = date;
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  });
+  let bookingsPerMonth = bookings.reduce((total, current) => {
+    const date = formatDate(current.createdAt, true);
+
+    const existingEntry = total.find((entry) => entry.date === date);
+    if (existingEntry) {
+      existingEntry.count += 1;
+    } else {
+      total.push({ date, count: 1 });
+    }
+    return total;
+  }, [] as Array<{ date: string; count: number }>);
+  return bookingsPerMonth;
 };
